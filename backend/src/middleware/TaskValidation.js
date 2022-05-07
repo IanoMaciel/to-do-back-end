4a5ea7a5b8c3817b23b826f0ab32a1d868dac307
IanoMaciel <ianomaciel6385@gmail.com>
2022-05-07 14:53:04 -0400

    Updated

--- backend/src/middleware/TaskValidation.js
+++ backend/src/middleware/TaskValidation.js
@@ -25,10 +25,18 @@ const TaskValidation = async (req, res, next) => {
     else {
         let exists;
 
-        exists = await TaskModel.findOne({
-            'when': {'$eq': new Date(when)},
-            'macaddress' : {'$in': macaddress}
-        });
+        if(req.params.id) {
+            exists = await TaskModel.findOne({
+                '_id': {'$ne': req.params.id},
+                'when': {'$eq': new Date(when)},
+                'macaddress' : {'$in': macaddress}
+            });
+        } else {
+            exists = await TaskModel.findOne({
+                'when': {'$eq': new Date(when)},
+                'macaddress' : {'$in': macaddress}
+            });
+        }
 
         if(exists) {
             return res.status(400).json({error: 'Já existe uma tarefa com a mesmo dia e hora'});
