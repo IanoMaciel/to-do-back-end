99634db64a2e32fa9a00684a49c20024bf7a1947
IanoMaciel <ianomaciel6385@gmail.com>
2022-05-07 20:20:34 -0400

    Add role to display a single task

--- backend/src/controller/TaskController.js
+++ backend/src/controller/TaskController.js
@@ -33,6 +33,18 @@ class TaskController {
             return res.status(500).json(error);                
         });
     }
+
+    async show(req, res) { // buscar apenas uma tarefa
+        await TaskModel.findById(req.params.id)
+        .then(response => {
+            if(response)
+                return res.status(200).json(response);
+            else return res.status(404).json({error: 'Tarefa não encontrada'});
+
+        }).catch(error => {
+            return res.status(500).json(error);                
+        });
+    }
 }
 
 module.exports = new TaskController();
