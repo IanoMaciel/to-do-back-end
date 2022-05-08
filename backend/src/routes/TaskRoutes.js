d81a4a0818ab10016fa111952c252aa77ec123d9
IanoMaciel <ianomaciel6385@gmail.com>
2022-05-07 22:31:09 -0400

    Route to update task status

--- backend/src/routes/TaskRoutes.js
+++ backend/src/routes/TaskRoutes.js
@@ -8,7 +8,8 @@ const MacAddressValidation = require('../middleware/MacAddressValidation');
 router.post('/', TaskValidation, TaskController.create);
 router.put('/:id', TaskValidation, TaskController.update);
 router.get('/:id', TaskController.show); // rota para listar apenas uma tarefa
-router.delete('/:id', TaskController.delete) // rota para deletar uma única tarefa por vez
+router.delete('/:id', TaskController.delete); // rota para deletar uma única tarefa por vez
+router.put('/:id/:done', TaskController.done); // atualizar os status da tarefa
 router.get('/filter/all', MacAddressValidation, TaskController.listAll ); //rota para listar todas as tarefas
 
 module.exports = router;
