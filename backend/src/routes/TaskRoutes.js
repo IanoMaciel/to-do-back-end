83c5d786dd9bf3134367cf67f6ce4f32888649a9
IanoMaciel <ianomaciel6385@gmail.com>
2022-05-07 20:04:56 -0400

    Modified

--- backend/src/routes/TaskRoutes.js
+++ backend/src/routes/TaskRoutes.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const TaskController = require('../controller/TaskController');
 const TaskValidation = require('../middleware/TaskValidation');
+const MacAddressValidation = require('../middleware/MacAddressValidation');
 
 router.post('/', TaskValidation, TaskController.create);
 router.put('/:id', TaskValidation, TaskController.update);
+router.get('/filter/all', MacAddressValidation, TaskController.listAll );
 
 module.exports = router;
