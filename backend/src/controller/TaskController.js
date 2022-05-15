9ddae3536f31e1ffdcbb99d156047a819eb72949
IanoMaciel <ianomaciel6385@gmail.com>
2022-05-15 12:08:55 -0400

    Add function today

--- backend/src/controller/TaskController.js
+++ backend/src/controller/TaskController.js
@@ -1,4 +1,8 @@
 const TaskModel = require('../model/TaskModel');
+const {
+    startOfDay,
+    endOfDay
+  } = require('date-fns');
 
 const current = new Date();
 
@@ -75,6 +79,23 @@ class TaskController {
         await TaskModel.find({
             'when': {'$lt': current},
             'macaddress': {'$in' : req.body.macaddress}
+        })
+        .sort('when')
+        .then(response => {
+            return res.status(200).json(response);
+
+        }).catch(error => {
+            return res.status(500).json(error);                
+        });
+    }
+
+    async today(req, res) {
+        await TaskModel.find({
+            'macaddress': {'$in': req.body.macaddress},
+            'when': {
+                '$gte': startOfDay(current),
+                '$lt': endOfDay(current)
+            } 
         })
         .sort('when')
         .then(response => {
