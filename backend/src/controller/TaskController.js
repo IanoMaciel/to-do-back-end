655141f9b77d5ac8fece0e6f721ed5ec61b17e9c
IanoMaciel <ianomaciel6385@gmail.com>
2022-05-15 18:43:23 -0400

    added the function to list all tasks for the years

--- backend/src/controller/TaskController.js
+++ backend/src/controller/TaskController.js
@@ -5,7 +5,9 @@ const {
     startOfWeek,
     endOfWeek,
     startOfMonth,
-    endOfMonth
+    endOfMonth,
+    startOfYear,
+    endOfYear
   } = require('date-fns');
 
 const current = new Date();
@@ -133,6 +135,23 @@ class TaskController {
             'when': {
                 '$gte': startOfMonth(current),
                 '$lt': endOfMonth(current)
+            } 
+        })
+        .sort('when')
+        .then(response => {
+            return res.status(200).json(response);
+
+        }).catch(error => {
+            return res.status(500).json(error);                
+        });
+    }
+
+    async year(req, res) {
+        await TaskModel.find({
+            'macaddress': {'$in': req.body.macaddress},
+            'when': {
+                '$gte': startOfYear(current),
+                '$lt': endOfYear(current)
             } 
         })
         .sort('when')
