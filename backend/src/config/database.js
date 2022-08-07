7d14aebc6b2e555fff533260efd3f4ba400ad4f0
IanoMaciel <ianomaciel6385@gmail.com>
2022-08-07 18:07:30 -0400

    project restructuring

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,3 +1,5 @@
+// script de conexão com o banco mongodb
+
 const mongoose = require('mongoose');
 const url = 'mongodb://localhost:27017/todo';
 mongoose.connect(url, { useNewUrlParser: true });
--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,7 @@ server.use(express.json())
 const TaskRoutes = require('./routes/TaskRoutes');
 server.use('/task', TaskRoutes);
 
+
 server.listen(3000, () => { //escuta uma determinada porta
     console.log('API ONLINE');
-}) 
+});
