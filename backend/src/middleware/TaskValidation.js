c88b5a92f670a65ee3f85dc1abac4611abf60ac1
IanoMaciel <ianomaciel6385@gmail.com>
2022-08-14 17:53:13 -0400

    Allow updating tasks in the past

--- backend/src/middleware/TaskValidation.js
+++ backend/src/middleware/TaskValidation.js
@@ -20,8 +20,6 @@ const TaskValidation = async (req, res, next) => {
         return res.status(400).json({error: 'Descrição é obrigatório'});
     else if(!when)
         return res.status(400).json({error: 'Data e hora são campos obrigatórios'});
-    else if(isPast(new Date(when)))
-        return res.status(400).json({error: 'Escolha uma data e hora futura'});
     else {
         let exists;
 
@@ -32,6 +30,9 @@ const TaskValidation = async (req, res, next) => {
                 'macaddress' : {'$in': macaddress}
             });
         } else {
+            if(isPast(new Date(when))) {
+                return res.status(400).json({error: 'Escolha uma data e hora futura'});
+            }
             exists = await TaskModel.findOne({
                 'when': {'$eq': new Date(when)},
                 'macaddress' : {'$in': macaddress}
